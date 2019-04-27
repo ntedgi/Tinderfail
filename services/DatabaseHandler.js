ad56b227cfbf7f7cabd7ff7fe20bb8829e5e9f57
molda <hbeygel@gmail.com>
2019-04-28 02:05:21 +0300

    implemented getting and setting answers to db

--- services/DatabaseHandler.js
+++ services/DatabaseHandler.js
@@ -1,11 +1,35 @@
 import database from "./FireBaseLoader";
 
-const addUserAnswers = (userName, answers) => {};
+const addUserAnswers = (userId, answers) => {
+  return database
+    .database()
+    .ref(`/predictions/${userId}`)
+    .set({
+      answers
+    });
+};
+
 const displayUsersScore = () => {};
-const getCurrentQuestionsAnswers = () => {};
+
+const getCurrentQuestionsAnswers = async userId => {
+  return new Promise(resolve => {
+    const answersRef = database
+      .database()
+      .ref(`/predictions/${userId}`)
+      .child("answers");
+      answersRef.on("value", function(snapshot) {
+        const val = snapshot.val();
+        resolve(val);
+      }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+        resolve(null)
+      })
+  });
+};
 
 const createUser = async (email, password) => {
   console.log(email);
+  console.log(database);
   return database
     .auth()
     .createUserWithEmailAndPassword(email, password)
@@ -38,5 +62,5 @@ const loginUser = async (email, password) => {
       }
       console.log(error);
     });
-}
+};
 export { displayUsersScore, addUserAnswers, getCurrentQuestionsAnswers, createUser, loginUser };
